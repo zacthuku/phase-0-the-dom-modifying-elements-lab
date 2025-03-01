295103661d371c5546f99d86eb0901559aee2158
Zacharia Ndinguri <zacthuku@gmail.com>
2025-03-01 10:24:20 +0300

    comlete lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your code here!
+
+window.onload = () => {
+    const element = document.getElementById("main");
+    if (element) {
+        element.remove();
+    }
+};
+const newHeader = document.createElement("h1");
+    newHeader.id = "victory"; // Assign an ID (optional)
+    newHeader.textContent = "ZACHARIA THUKU is the champion"; // Set its text content
+    
+
+    // Append the new <h1> to the document body
+    document.body.appendChild(newHeader);
+
+    // Make newHeader accessible in the global scope
+    window.newHeader = newHeader;
+
+
